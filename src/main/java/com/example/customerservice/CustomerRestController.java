f56198b9d35fdef6f134e6839349d8d05cfda064
Alberto M. Perez <alberto.perez@oramon.nl>
2018-06-02 00:29:49 +0200

    Add rest service test

--- src/main/java/com/example/customerservice/CustomerRestController.java
+++ src/main/java/com/example/customerservice/CustomerRestController.java
@@ -1,6 +1,8 @@
 package com.example.customerservice;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -16,5 +18,11 @@ class CustomerRestController {
 	@GetMapping("/customers")
     Collection<Customer> customers(){
 		return this.customerRepository.findAll();
+	}
+
+	@GetMapping(path = "/customers/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Customer customerByid(@PathVariable Long id){
+		return this.customerRepository.findById(id).orElse(new Customer());
+
 	}
 }
---
+++ src/test/java/com/example/customerservice/CustomerRestControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.customerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class CustomerRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void customerByIdShouldReturnAJsonCustomer() throws Exception {
+        Mockito.when(this.customerRepository.findById(1L))
+                .thenReturn(Optional.of(new Customer(1L, "al", "be", "a@a.com")));
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/customers/1"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.id").value(1L))
+                .andReturn();
+
+    }
+
+    @Test
+    public void customersShouldReturnAllCustomers() throws Exception {
+
+        Mockito.when(this.customerRepository.findAll())
+                .thenReturn(Arrays.asList(
+                        new Customer(1L, "first", "be", "a@a.com"),
+                        new Customer(2L, "two", "be", "a@a.com")));
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/customers"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[0].firstName").value("first"))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[1].id").value(2L))
+                .andReturn();
+
+    }
+}
+
