ccf8c663f8da99e5e15d2aa014e821c763a0578a
Alberto M. Perez <alberto.perez@oramon.nl>
2018-06-03 20:23:11 +0200

    Add html documentation auto generated from the rest controller test
    target/generated-docs/api-guide.html is generated you can see it on your browser
    
    now the doc matches the test

--- src/test/java/com/example/customerservice/CustomerRestControllerTest.java
+++ src/test/java/com/example/customerservice/CustomerRestControllerTest.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,6 +23,8 @@ import java.util.Optional;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+@AutoConfigureJsonTesters
 public class CustomerRestControllerTest {
 
     @Autowired
@@ -55,6 +60,7 @@ public class CustomerRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("@.[0].id").value(1L))
                 .andExpect(MockMvcResultMatchers.jsonPath("@.[0].firstName").value("first"))
                 .andExpect(MockMvcResultMatchers.jsonPath("@.[1].id").value(2L))
+                .andDo(MockMvcRestDocumentation.document("customers"))
                 .andReturn();
 
     }
